6f7ba19d98f387409201b185a01bd75ab3c37b92
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-07-08 16:51:26 -0300

    Implement Sidebar component and Home page

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,72 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
 import classNames from 'classnames';
+import { gql,useQuery } from '@apollo/client';
 
-const lotteries = ['mega-sena', 'quina', 'lotofácil', 'lotomania', 'timemania', 'dia-de-sorte']
+const GET_LOTTERIES_QUERY = gql`
+  query getLotteries {
+    loterias {
+      id
+      nome
+    },
+    loteriasConcursos {
+      loteriaId
+      concursoId
+    }
+  }
+`;
 
-export function Sidebar() {
-  const [selectedLottery, setSelectedLoterry] = useState(lotteries[0]);
+interface GetLotteriesResponse {
+  loterias: {
+    id: number;
+    nome: string;
+  }[],
+  loteriasConcursos: {
+    loteriaId: number;
+    concursoId: string;
+  }[],
+}
+
+interface SidebarProps  {
+  changeLotteryContest: (concursoId: String) => void;
+  lotteryContest: {
+    id?: string;
+    date?: Date;
+  }
+}
+
+export function Sidebar({ changeLotteryContest, lotteryContest }: SidebarProps) {
+  const { data, loading } = useQuery<GetLotteriesResponse>(GET_LOTTERIES_QUERY);
+  const [selectedLottery, setSelectedLottery] = useState('mega-sena');
+
+
+  useEffect(() => {
+    const findLotteryId = data?.loterias.find(lottery => lottery.nome === selectedLottery);
+
+    const findContestId = data?.loteriasConcursos.find(contest => contest.loteriaId === findLotteryId?.id);
+
+    if (findContestId) {
+      changeLotteryContest(findContestId.concursoId);
+    }
+
+  }, [selectedLottery])
+
+  const formatDate = lotteryContest.date ? new Date(lotteryContest.date).toLocaleString(
+    'pt-BR',
+    {
+      day: '2-digit',
+      month: 'numeric',
+      year: 'numeric'
+    }
+  ) : null
+ 
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center w-[613px] text-gray-dark ">
+        <p className="text-3xl">Carregando...</p>
+      </div>
+    )
+  }
 
   return (
     <aside className={classNames('flex flex-col justify-between px-24 py-[5.75rem] w-[613px]', {
@@ -14,9 +75,9 @@ export function Sidebar() {
       'bg-loto-facil': selectedLottery === 'lotofácil', 
       'bg-loto-mania':selectedLottery === 'lotomania',
       'bg-time-mania': selectedLottery === 'timemania',
-      'bg-dia-de-sorte': selectedLottery === 'dia-de-sorte'
+      'bg-dia-de-sorte': selectedLottery.replace(/ /g, '-') === 'dia-de-sorte'
     })}>
-      <Listbox value={selectedLottery} onChange={setSelectedLoterry}>
+      <Listbox value={selectedLottery} onChange={setSelectedLottery}>
         <Listbox.Button className="relative w-[215px] cursor-default rounded-lg bg-white py-[0.938rem] pl-[1.438rem] text-left font-medium text-gray-dark shadow-md focus:outline-none focus-visible:border-gray-dark focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-dark text-[0.938rem]">
           <span className="block truncate uppercase">{selectedLottery}</span>
           <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-[1.188rem]">
@@ -30,10 +91,10 @@ export function Sidebar() {
           leaveTo="opacity-0" 
         >
           <Listbox.Options className="absolute mt-[3rem] w-[215px] overflow-auto rounded-b-md bg-white py-1 font-medium text-gray-dark text-[0.938rem] shadow-md ring-1 ring-black ring-opacity-5 focus:outline-none">
-            {lotteries.map(lottery => (
+            {data?.loterias.map(lottery => (
               <Listbox.Option 
-                key={lottery} 
-                value={lottery}
+                key={lottery.id} 
+                value={lottery.nome}
                 className={({ active }) =>
                   `relative cursor-default select-none py-2 pl-10 
                   ${active ? 'bg-gray-light text-black' : 'text-gray-dark'}`
@@ -42,11 +103,11 @@ export function Sidebar() {
                 {({ selected }) => (
                   <>
                     <span
-                      className={`block truncate ${
+                      className={`block truncate uppercase ${
                         selected ? 'font-medium' : 'font-normal'
                       }`}
                     >
-                      {lottery}
+                      {lottery.nome}
                     </span>
                     {selected ? (
                       <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-black">
@@ -68,7 +129,7 @@ export function Sidebar() {
 
       <div>
         <span className="block text-sm text-white font-medium mb-[0.875rem] tracking-widest uppercase">Concurso</span>
-        <span className="block text-[1.25rem] leading-6 text-white font-bold">4531 – 07/04/2020</span>
+        <span className="block text-[1.25rem] leading-6 text-white font-bold">{`${lotteryContest?.id} - ${formatDate}`}</span>
       </div>
     </aside>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,37 @@
+import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
 import { LotteryResults } from "../components/LotteryResults";
 import { Sidebar } from "../components/Sidebar"
 
+const GET_LOTTERY_CONTEST_QUERY = gql`
+  query GetLotteryContest ($id: ID!) {
+    concurso (id: $id) {
+      id
+      numeros
+      data
+    }
+  }`;
+
+interface GetLotteryContestResponse {
+  concurso: {
+    id: string;
+    numeros: String[];
+    data: Date; 
+  }
+}
+
 export function Home() {
-  return(
+  const [lotteryContest, setLotteryContest] = useState<String>("2359");
+  const { data, loading } = useQuery<GetLotteryContestResponse>(GET_LOTTERY_CONTEST_QUERY, {
+    variables: {
+      id: lotteryContest
+    }
+  });
+
+  return (
     <div className="flex min-h-screen">
-      <Sidebar />
-      <LotteryResults />
+      <Sidebar changeLotteryContest={setLotteryContest} lotteryContest={{ id: data?.concurso.id, date: data?.concurso.data}} />
+      <LotteryResults results={data?.concurso.numeros} loading={loading}/>
     </div>
   );
 }
