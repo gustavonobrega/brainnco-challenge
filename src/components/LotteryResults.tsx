e81c075a51e6311f569c80792ea7bb39245f7a7b
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-07-08 16:51:46 -0300

    Implement LotteryResults component

--- src/components/LotteryResults.tsx
+++ src/components/LotteryResults.tsx
@@ -1,22 +1,32 @@
 
-const lotteryResults = [6, 9, 28, 33, 37, 40];
+interface LotteryResultsProps {
+    results?: String[];
+    loading?: boolean;
+}
 
-export function LotteryResults() {
+export function LotteryResults({ results, loading }: LotteryResultsProps) {
+
+  if (loading) {
+    return (
+      <main className=" flex justify-center items-center flex-1 text-gray-dark bg-gray-light">
+        <h1 className="text-3xl">Carregando...</h1>
+      </main>
+    )
+  }
+
   return (
-    <main className=" flex justify-center items-end flex-1 bg-gray-light py-[6.063rem]" >
-      <div className="flex flex-col items-center gap-y-[16.75rem]" >
-        <ul className="flex gap-[2.188rem]">
-            {lotteryResults.map(result => (
-              <li className="flex items-center justify-center h-[6.938rem] w-[6.625rem] rounded-full bg-white">
-                <span className="text-[1.688rem] font-bold leading-8 text-gray-dark ">{result}</span>
-              </li>
-            ))}
-          </ul>
+    <main className=" flex flex-col justify-center items-center flex-1 bg-gray-light px-7 py-[6.063rem]">
+        <ul className="flex flex-wrap gap-[2.188rem] mt-auto">
+          {results?.map(result => (
+            <li key={Number(result)} className="flex items-center justify-center h-[6.938rem] w-[6.625rem] rounded-full bg-white">
+              <span className="text-[1.688rem] font-bold leading-8 text-gray-dark">{result}</span>
+            </li>
+          ))}
+        </ul>
 
-          <p className="leading-5 text-black">
-            Este sorteio é meramente ilustrativo e não possui nenhuma ligação com a CAIXA.
-          </p>
-      </div>
+        <p className="leading-5 text-black mt-auto">
+          Este sorteio é meramente ilustrativo e não possui nenhuma ligação com a CAIXA.
+        </p>
     </main>
   );
 }
